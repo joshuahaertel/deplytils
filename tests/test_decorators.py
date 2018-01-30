ab93a0c0f54bfe7625898a4aa5b47933c3c8eed0
Joshua Haertel <josh.haertel@gmail.com>
2018-01-29 20:20:21 -0700

    Add tests for full decorator coverage

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -32,3 +32,14 @@ class TestCachedClassProperty(TestCase):
     def test_instance_has_access(self):
         self.assertEqual(self.instance.property, 1)
         self.assertEqual(self.instance.property, 1)
+
+    def test_full_delete_class_property(self):
+        del self.class_.property
+        with self.assertRaises(AttributeError):
+            del self.class_.property
+
+    def test_delete_class_property_after_use(self):
+        """make sure we can delete class property after using it"""
+        self.test_cache_invalidates()
+        del self.class_.property
+        self.test_full_delete_class_property()

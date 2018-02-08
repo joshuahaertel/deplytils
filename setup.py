4cbbee44ccccd23c4c3add71e087694044739066
Joshua Haertel <josh.haertel@gmail.com>
2018-02-08 00:31:56 -0700

    Add tox files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='deplytils',
+    version='0.0.0',
+    packages=['putils', 'putils.contexts', 'putils.extensions'],
+    url='https://github.com/joshuahaertel/putils',
+    license='MIT',
+    author='Joshua Haertel',
+    author_email='josh.haertel@gmail.com',
+    description='Python tools used to simplify CI'
+)
